c8504335d164c7da3b670c342175956b1ab329db
Stajah Lee Hoeflich <stajah@stajahlee.com>
2020-03-28 17:36:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbc7c790f05acdfc8bd346bad26d9373.js"
+  "/precache-manifest.f76ec3a9c956ba68b58162efd51aa11b.js"
 );
 
 self.addEventListener('message', (event) => {
