42279e69b9d590f69f41476c2c31ed14a508c8c1
stajah <stajahleehoeflich@gmail.com>
2024-08-07 17:02:16 -0400

    test and finish 404 page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,39 @@
-'use client';
-import React, { useState } from 'react';
-import MenuBarMobile from './components/layout/mobile/menu-bar-mobile';
-import Sidebar from './components/layout/sidebar/sidebar';
+import React from 'react';
+import Link from 'next/link';
+
+const navigation = [
+  { name: 'Projects', href: '/projects' },
+  { name: 'Contact', href: '/contact' },
+];
 
 export default function Home() {
-  const [showSidebar, setShowSidebar] = useState(false);
-
   return (
-    <div className="flex min-h-screen">
-      <MenuBarMobile setter={() => setShowSidebar(!showSidebar)} />
-      <Sidebar show={showSidebar} setter={() => setShowSidebar(!showSidebar)} />
-      <div className="flex flex-col justify-center ms-5 flex-grow md:w-full min-h-screen mr-[40px]">
-        <div className="hidden h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-        <h5 className="pb-4 text-4xl text-transparent duration-800 bg-white cursor-default animate-title font-display sm:text-6xl md:text-6xl whitespace-nowrap bg-clip-text ">
-          stajah lee hoeflich
-        </h5>
-        <div className="hidden h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+    <div className="overflow-hidden bg-gradient-to-br from-black to-neutral-900">
+      <div className="body-background flex flex-col items-center justify-center w-screen h-screen">
+        <nav className="my-16 animate-fade-in">
+          <ul className="flex items-center justify-center gap-4">
+            {navigation.map((item) => (
+              <Link
+                key={item.href}
+                href={item.href}
+                className="text-sm duration-500 text-zinc-400 hover:text-white"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <h1 className="py-3.5 px-0.5 z-10 text-3xl text-transparent duration-1000 bg-white cursor-default animate-title font-display sm:text-5xl md:text-6xl whitespace-nowrap bg-clip-text ">
+        stajah lee hoeflich
+        </h1>
+
+        <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+        <div className="my-16 text-center animate-fade-in">
+          <h2 className="text-sm text-zinc-300 ">
+            {'It\'s not a bug - it\' s an undocumented feature.'}
+          </h2>
+        </div>
       </div>
     </div>
   );
