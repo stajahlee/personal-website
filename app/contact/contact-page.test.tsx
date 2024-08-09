819c97020c166409f5efd89ff9a4e8acd00a2680
stajah <stajahleehoeflich@gmail.com>
2024-08-09 19:15:16 -0400

    test contact page

--- app/contact/contact-page.test.tsx
+++ app/contact/contact-page.test.tsx
@@ -1,22 +1,40 @@
 import '@testing-library/jest-dom'
-import { render, within } from '@testing-library/react'
+import { fireEvent, render, within } from '@testing-library/react'
 import ContactPage from './page'
  
 describe('ContactPage', () => {
   const subject = () => {
-    return render(<ContactPage />)
+    const rendered = render(<ContactPage />)
+    const socialCards = rendered.container.querySelectorAll('#card')
+    
+    return {
+      ...rendered,
+      socialCards
+    }
   }
 
-  it('renders go back arrow link', () => {
-    const { getByRole } = subject()
- 
-    const goBackArrow = getByRole('button', { name: 'Go back' })
-    expect(goBackArrow.firstChild).toHaveClass('lucide-arrow-left')
+  it('renders linkedin card and link', () => {
+    const { socialCards } = subject()
+    const linkedInCard = socialCards[0] as HTMLElement
+    
+    expect(socialCards).toHaveLength(3)
+    expect(linkedInCard).toHaveTextContent('LinkedIn')
+    expect(within(linkedInCard).getByRole('img').firstChild).toHaveClass('lucide-linkedin')
   })
 
-  it.only('renders navigation links', () => {
-    const { debug } = subject()
- 
-    debug()
+  it('renders github card and link', () => {
+    const { socialCards } = subject()
+    const githubCard = socialCards[1] as HTMLElement
+    
+    expect(githubCard).toHaveTextContent('Github')
+    expect(within(githubCard).getByRole('img').firstChild).toHaveClass('lucide-github')
+  })
+
+  it('renders email card and link', () => {
+    const { socialCards } = subject()
+    const emailCard = socialCards[2] as HTMLElement
+    
+    expect(emailCard).toHaveTextContent('Email')
+    expect(within(emailCard).getByRole('img').firstChild).toHaveClass('lucide-mail')
   })
 })
